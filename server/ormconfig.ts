630e56f8e28a098bb6c3dcbc78efb5658b25da03
Sigit Ari Prasetyo <sigitari111@gmial.com>
2021-02-21 00:56:37 +0700

    add env example

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,26 +1,3 @@
-// {
-//   "type": "postgres",
-//   "host": "localhost",
-//   "port": 5432,
-//   "username": "postgres",
-//   "password": "postgres",
-//   "database": "pos",
-//   "synchronize": false,
-//   "logging": false,
-//   "entities": [
-//     "src/entities/**/*.ts"
-//   ],
-//   "migrations": [
-//     "src/migration/**/*.ts"
-//   ],
-//   "subscribers": [
-//     "src/subscriber/**/*.ts"
-//   ],
-//   "migrationsTableName": "migration_table",
-//   "cli": {
-//     "migrationsDir": "./src/migration"
-//   }
-// }
 export = {
   type: "postgres",
   host: process.env.DB_HOST!,
