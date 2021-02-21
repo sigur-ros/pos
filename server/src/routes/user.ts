6d3bb6ba30cdd32bc36e8248bc580664dacbc49e
Kevin Anantha <kevanantha@evilfactory.id>
2021-02-21 21:56:00 +0700

    chore: remove server (#6)

--- server/src/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express'
-import UserController from '../controller/User'
-const User: Router = express.Router()
-
-User.post('/create', UserController.create)
-User.get('/get-user', UserController.getUser)
-
-export default User
